e715daafb556029bd1d827184d3581298f9ad8a4
gomeznaiker <gomeznaiker@gmail.com>
2025-06-10 16:12:44 +0000

    fix: Produc

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config'
 import tailwind from "@astrojs/tailwind"
-
 import robotsTxt from "astro-robots-txt"
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://naiker12.github.io',
-  base: '/Portfolio-Code/', 
-  outDir: 'www',
+  site: 'https://naiker12.github.io',        
+  // base: '/Portfolio-Code/',                 
+  outDir: 'www',                           
 })
