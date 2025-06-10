f05ae634eeb1ec7c030c94edfa4208f41df4c141
gomeznaiker <gomeznaiker@gmail.com>
2025-06-10 15:08:35 +0000

    test: Prueba de serve

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import robotsTxt from "astro-robots-txt"
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
+  site: 'https://naiker12.io',
   site: 'https://porfolio.dev/',
+  base: '/Portfolio-Code/', 
   outDir: 'www',
-  output: 'static',
 })
