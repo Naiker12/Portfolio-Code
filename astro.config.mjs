51b1e953864e8ada4607b81e8f8b59ff3be5af2a
gomeznaiker <gomeznaiker@gmail.com>
2025-06-13 14:09:19 +0000

    fix: add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from 'astro/config'
-import tailwind from "@astrojs/tailwind"
-import robotsTxt from "astro-robots-txt"
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
   site: 'https://porfolio.dev/',
   adapter: vercel(),                                               
-})
+});
