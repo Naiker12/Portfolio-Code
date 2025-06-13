2b789ea07352bdb7d13ec9c451db84b5f3c640a5
gomeznaiker <gomeznaiker@gmail.com>
2025-06-13 14:20:26 +0000

    fix: Prueba

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import robotsTxt from "astro-robots-txt";
+import tailwind from '@astrojs/tailwind';
+import robotsTxt from 'astro-robots-txt';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
   site: 'https://porfolio.dev/',
-  adapter: vercel(),                                               
+  integrations: [
+    tailwind(),
+    robotsTxt(),
+  ],
+  adapter: vercel(),
 });
