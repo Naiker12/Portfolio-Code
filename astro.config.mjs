567823d0e60ec1a54f14145f56ac0b6cba2d288a
gomeznaiker <gomeznaiker@gmail.com>
2025-06-13 13:48:54 +0000

    Despliegue a vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config'
 import tailwind from "@astrojs/tailwind"
 import robotsTxt from "astro-robots-txt"
+import vercel from "@astrojs/vercel/serverless"
 
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://naiker12.github.io',        
-  // base: '/Portfolio-Code/',                 
-  outDir: 'www',                           
+  output: 'server',
+  adapter: vercel(),                                               
 })
